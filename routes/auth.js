9fa07a988317b58a9b5f8bff0b3ecd8eeb026a96
Bernardo-Quina <bernardoquina@gmail.com>
2020-10-08 16:47:11 +0100

    Forgot Password - Generate Token

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const { register, login, getMe } = require('../controllers/auth');
+const {
+    register,
+    login,
+    getMe,
+    forgotPassword
+} = require('../controllers/auth');
 
 const router = express.Router();
 
@@ -13,5 +18,7 @@ router
 
 router
     .get('/me', protect, getMe);
+
+router.post('/forgotpassword', forgotPassword);
 
     module.exports = router;
