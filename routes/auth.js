9ace444ebb76b35da314c7e651740847c48dbff1
Bernardo-Quina <bernardoquina@gmail.com>
2020-10-08 20:40:29 +0100

    Reset Password

--- routes/auth.js
+++ routes/auth.js
@@ -3,22 +3,22 @@ const {
     register,
     login,
     getMe,
-    forgotPassword
+    forgotPassword,
+    resetPassword
 } = require('../controllers/auth');
 
 const router = express.Router();
 
 const { protect } = require('../middleware/auth');
 
-router
-    .post('/register', register);
+router.post('/register', register);
 
-router
-    .post('/login', login);
+router.post('/login', login);
 
-router
-    .get('/me', protect, getMe);
+router.get('/me', protect, getMe);
 
 router.post('/forgotpassword', forgotPassword);
+
+router.put('/resetpassword/:resettoken', resetPassword);
 
     module.exports = router;
