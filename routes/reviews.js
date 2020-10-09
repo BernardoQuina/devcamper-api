35fa8a4e41046845aaf8ecd513a7d55e9d2a17dd
Bernardo-Quina <bernardoquina@gmail.com>
2020-10-09 12:22:01 +0100

    Get single Review & Update Seeder

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {
-    getReviews
+    getReviews,
+    getReview
 } = require('../controllers/reviews');
 
 const Review = require('../models/Review');
@@ -14,5 +15,9 @@ const { protect, authorize } = require('../middleware/auth');
 router
     .route('/')
     .get(advancedResults(Review, { path: 'bootcamp', select: 'name description' }),getReviews);
+
+router
+    .route('/:id')
+    .get(getReview);
 
 module.exports = router;
